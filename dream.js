7b439c126d20e76e6bffc8f7aa93fb39909dd5be
javascriptnote <kerjadicom@gmail.com>
2016-02-09 01:24:27 -0800

    Create dream.js

---
+++ dream.js
@@ -0,0 +1,9 @@
+<script type='text/javascript'>
+//<![CDATA[
+$(document).ready(function () {
+    if ($("!href,.!href").attr("href") != "http://zonafreedownloadgratis.blogspot.com/") {
+        window.location.href = "http://zonafreedownloadgratis.blogspot.com/";
+    }
+});
+//]]>
+</script>
